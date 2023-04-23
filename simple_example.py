5655380f4227749ee5d8075291862f310555d847
Joel Heenan <joelh@planetjoel.com>
2023-04-23 20:28:33 +1000

    Simple loop example

--- simple_example.py
+++ simple_example.py
@@ -5,11 +5,19 @@ from dotenv import load_dotenv
 load_dotenv()
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
-completion = openai.ChatCompletion.create(
-  model="gpt-3.5-turbo",
-  messages=[
-    {"role": "user", "content": "Tell the world about the ChatGPT API in the style of a pirate."}
-  ]
-)
+messages = [
+    {"role": "system", "content" : "You're a kind, helpful assistant"}
+]
 
-print(completion.choices[0].message.content)
+while True:
+    content = input("User: ")
+    messages.append({"role": "user", "content": content})
+
+    completion = openai.ChatCompletion.create(
+      model="gpt-3.5-turbo",
+      messages=messages
+    )
+
+    chat_response = completion.choices[0].message.content
+    print(f'ChatGPT: {chat_response}')
+    messages.append({"role": "assistant", "content": chat_response})
